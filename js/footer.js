398a5bc1a14f8c0d7a64a9f751296c1b201d4320
BilalAhmad096 <bilal.ahmad096@gmail.com>
2025-11-10 02:11:04 +0530

    Collaborators added

--- js/footer.js
+++ js/footer.js
@@ -4,29 +4,45 @@
 
   const html = `
 <footer class="site-footer">
-  <div class="container">
-    <div class="footer-right">
-      <a class="social" href="https://www.linkedin.com/in/mintorian" target="_blank" rel="noopener">
-        <img class="social-icon" src="${base}/footer/icon-linkedin.svg" alt="LinkedIn">
-      </a>
-      <a class="social" href="https://github.com/BilalAhmad096" target="_blank" rel="noopener">
-        <img class="social-icon" src="${base}/footer/icon-github.svg" alt="GitHub">
-      </a>
-      <a class="social" href="https://scholar.google.com/citations?user=Y-ShcXcAAAAJ" target="_blank" rel="noopener">
-        <img class="social-icon" src="${base}/footer/icon-scholar.svg" alt="Google Scholar">
-      </a>
-      <a class="social" href="https://x.com/ahmadbilal096" target="_blank" rel="noopener">
-        <img class="social-icon" src="${base}/footer/icon-x.svg" alt="X (Twitter)">
-      </a>
-      <a class="social" href="https://www.instagram.com/thebilalgram/" target="_blank" rel="noopener">
-        <img class="social-icon" src="${base}/footer/icon-instagram.svg" alt="Instagram">
-      </a>
-      <a class="social" href="https://leetcode.com/u/mailboxforbilal/" target="_blank" rel="noopener">
-        <img class="social-icon" src="${base}/footer/icon-leetcode.svg" alt="LeetCode">
-      </a>
-      <a class="social" href="https://www.hackerrank.com/profile/mailboxforbilal" target="_blank" rel="noopener">
-        <img class="social-icon" src="${base}/footer/icon-hackerrank.svg" alt="HackerRank">
-      </a>
+  <div class="container-wide">
+    <!-- Top row: collaborators' logos -->
+    <div class="footer-top">
+      <div class="footer-collab-row">
+        <span class="footer-label">Collaborators:</span>
+        <ul class="brand-logos footer-logos">
+          <li><a href="https://www.bath.ac.uk/" target="_blank"><img src="${base}/image/Bath Logo.gif" alt="University of Bath"></a></li>
+          <li><a href="https://www.tneigroup.com/" target="_blank"><img src="${base}/image/TNEI-logo.jpg" alt="TNEI Consultancy"></a></li>
+          <li><a href="https://www.royalholloway.ac.uk/" target="_blank"><img src="${base}/image/rhul.jpg" alt="Royal Holloway University of London"></a></li>
+          <li><a href="https://www.iitr.ac.in/" target="_blank"><img src="${base}/image/iitr.png" alt="IIT Roorkee"></a></li>
+          <li><a href="https://www.rwth-aachen.de/" target="_blank"><img src="${base}/image/rwth.png" alt="RWTH Aachen University"></a></li>
+          <li><a href="https://www.amu.ac.in/" target="_blank"><img src="${base}/image/amu.jpg" alt="Aligarh Muslim University"></a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="footer-bottom">
+      <div class="footer-right">
+        <a class="social" href="https://www.linkedin.com/in/mintorian" target="_blank" rel="noopener">
+          <img class="social-icon" src="${base}/footer/icon-linkedin.svg" alt="LinkedIn">
+        </a>
+        <a class="social" href="https://github.com/BilalAhmad096" target="_blank" rel="noopener">
+          <img class="social-icon" src="${base}/footer/icon-github.svg" alt="GitHub">
+        </a>
+        <a class="social" href="https://scholar.google.com/citations?user=Y-ShcXcAAAAJ" target="_blank" rel="noopener">
+          <img class="social-icon" src="${base}/footer/icon-scholar.svg" alt="Google Scholar">
+        </a>
+        <a class="social" href="https://x.com/ahmadbilal096" target="_blank" rel="noopener">
+          <img class="social-icon" src="${base}/footer/icon-x.svg" alt="X (Twitter)">
+        </a>
+        <a class="social" href="https://www.instagram.com/thebilalgram/" target="_blank" rel="noopener">
+          <img class="social-icon" src="${base}/footer/icon-instagram.svg" alt="Instagram">
+        </a>
+        <a class="social" href="https://leetcode.com/u/mailboxforbilal/" target="_blank" rel="noopener">
+          <img class="social-icon" src="${base}/footer/icon-leetcode.svg" alt="LeetCode">
+        </a>
+        <a class="social" href="https://www.hackerrank.com/profile/mailboxforbilal" target="_blank" rel="noopener">
+          <img class="social-icon" src="${base}/footer/icon-hackerrank.svg" alt="HackerRank">
+        </a>
+      </div>
     </div>
   </div>
 </footer>`;
